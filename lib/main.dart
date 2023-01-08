836609757ba04a09105f55feaf3c92d8d170496f
BarkaBoss <bossmajor.biii@gmail.com>
2023-01-08 01:19:54 +0100

    Formatted insert texts

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,14 @@ Future<void> main() async{
 
   (await db.select(db.staff).get()).forEach(print);
 
-  await db.into(db.staff).insert(StaffCompanion.insert(name: "Sarki Amada", gender: "Male", position: "CEO", staffID: "126VG87ABJ", dateOfBirth: "20/10/1972", imageBaseSixFour: ImageStrings.anotherStaffImage));
-  await db.into(db.staff).insert(StaffCompanion.insert(name: "Rose Amada", gender: "Female", position: "SEO", staffID: "126VG87ABJ", dateOfBirth: "20/10/2006", imageBaseSixFour: ImageStrings.ceo));
+  await db.into(db.staff).insert(StaffCompanion.insert(name: "Sarki Amada",
+      gender: "Male", position: "CEO",
+      staffID: "126VG87ABJ", dateOfBirth: "20/10/1972",
+      imageBaseSixFour: ImageStrings.anotherStaffImage));
+
+  await db.into(db.staff).insert(StaffCompanion.insert(name: "Rose Amada",
+      gender: "Female", position: "SEO", staffID: "126VG87ABJ",
+      dateOfBirth: "20/10/2006", imageBaseSixFour: ImageStrings.ceo));
+
   (await db.select(db.staff).get()).forEach(print);
 }
